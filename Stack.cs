d3e534f2b5037a4170535cc5320e611d5095405f
Dmitriy Mashukov <mashukov1988@gmail.com>
2025-03-26 18:07:33 +0700

    added the main task

---
+++ Stack.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtusHomework5
+{
+    internal class Stack
+    {
+        private List<string> _storage;
+
+        public Stack(params string[] str)
+        {
+            _storage = new List<string>(str);
+        }
+
+        public void Add(string str)
+        {
+            _storage.Add(str);
+        }
+
+        public string Pop()
+        {
+            if (_storage.Count == 0)
+                throw new ArgumentException("Стек пустой");
+
+            int lastIndex = _storage.Count - 1;
+            string element = _storage[lastIndex];
+            _storage.RemoveAt(lastIndex);
+            return element;
+        }
+
+        public int Size
+        {
+            get { return _storage.Count; }
+        }
+
+        public string Top
+        {
+            get
+            {
+                if (_storage.Count == 0)
+                    return null;
+
+                return _storage[_storage.Count - 1];
+            }
+        }
+    }
+}
