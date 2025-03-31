709f74f058db46a2e546698f6fcc9d6c2c713f3b
Dmitriy Mashukov <mashukov1988@gmail.com>
2025-03-31 16:56:27 +0700

    added additional task 3

--- Stack.cs
+++ Stack.cs
@@ -8,43 +8,59 @@ namespace OtusHomework5
 {
     public class Stack
     {
-        private List<string> _storage;
+        private StackItem _top;
+
+        private class StackItem
+        {
+            public string Value { get; }
+            public StackItem Previous { get; }
+
+            public StackItem(string value, StackItem previous)
+            {
+                Value = value;
+                Previous = previous;
+            }
+        }
 
         public Stack(params string[] str)
         {
-            _storage = new List<string>(str);
+            foreach (string s in str) 
+                Add(s);
         }
 
         public void Add(string str)
         {
-            _storage.Add(str);
+            _top = new StackItem(str, _top);
         }
 
         public string Pop()
         {
-            if (_storage.Count == 0)
+            if (_top == null)
                 throw new ArgumentException("Стек пустой");
 
-            int lastIndex = _storage.Count - 1;
-            string element = _storage[lastIndex];
-            _storage.RemoveAt(lastIndex);
-            return element;
+            string value = _top.Value;
+            _top = _top.Previous;
+            return value;
         }
 
         public int Size
         {
-            get { return _storage.Count; }
+            get 
+            {
+                int count = 0;
+                StackItem current = _top;
+                while (current != null)
+                {
+                    count++;
+                    current = current.Previous;
+                }
+                return count;
+            }
         }
 
         public string Top
         {
-            get
-            {
-                if (_storage.Count == 0)
-                    return null;
-
-                return _storage[_storage.Count - 1];
-            }
+            get { return _top?.Value;}
         }
 
         public static Stack Concat(params Stack[] stacks)
